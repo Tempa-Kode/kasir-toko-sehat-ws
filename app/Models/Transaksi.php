59ddb89a0b957119e0930f7d705d83be7121b875
Henry Jr Zai <hjuniorzai@gmail.com>
2025-09-14 18:01:08 +0700

    add : make model transaksi and their relationship

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaksi extends Model
+{
+    protected $table = 'tb_transaksi';
+
+    protected $fillable = [
+        'no_nota',
+        'tgl_transaksi',
+        'harga_total',
+        'kasir_id',
+    ];
+
+    public function kasir() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'kasir_id');
+    }
+}
---
+++ database/migrations/2025_09_14_105247_create_transaksis_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_nota', 20)->unique();
+            $table->dateTime('tgl_transaksi');
+            $table->decimal('harga_total', 10, 2);
+            $table->foreignId('kasir_id')
+                ->constrained('tb_pengguna')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_transaksi');
+    }
+};
